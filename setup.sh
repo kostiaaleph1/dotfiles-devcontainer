79411132e567999161466669bb295337a5940efc
Kostiantyn Lelikov <kostiantyn.lelikov@eze.tech>
2025-02-14 14:12:06 +0200

    basic setup

--- setup.sh
+++ setup.sh
@@ -62,6 +62,8 @@ NVIM_DATA_DIR=/root/.local/share/nvim
 mkdir -p $ROOT_CONFIG_DIR
 mkdir -p $NVIM_DATA_DIR
 
+cd /root
+
 git clone https://github.com/kostiaLelikov1/nvim ${ROOT_CONFIG_DIR}
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
