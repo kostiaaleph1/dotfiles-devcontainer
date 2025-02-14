079b736774caebd23f4af3546d545d133701fc4c
Kostiantyn Lelikov <kostiantynlelikov@Kostiantyns-MacBook-Pro.local>
2025-02-14 11:39:35 +0200

    basic setup

--- setup.sh
+++ setup.sh
@@ -30,16 +30,18 @@ sudo apt install -y neovim
 # Create necessary directories
 echo "Creating Neovim configuration directories..."
 NVIM_CONFIG_DIR="/home/vscode/.config/nvim"
-mkdir -p "$NVIM_CONFIG_DIR"
+NVIM_DATA_DIR="/home/vscode/.local/share/nvim"
 
-export XDG_CONFIG_HOME="$NVIM_CONFIG_DIR"
+mkdir -p "$NVIM_CONFIG_DIR"
+mkdir -p "$NVIM_DATA_DIR"
 
 # Clone your Neovim configuration
 echo "Cloning your Neovim configuration..."
 git clone https://github.com/kostiaLelikov1/nvim.git "$NVIM_CONFIG_DIR"
 
 # Set correct permissions
-sudo chown -R vscode:vscode "$NVIM_CONFIG_DIR"
+sudo chown -R vscode:vscode /home/vscode/.local
+sudo chown -R vscode:vscode /home/vscode/.config
 
 # Set up executable links
 sudo ln -sf $(which fdfind) /usr/local/bin/fd
@@ -48,7 +50,6 @@ sudo ln -sf $(which fdfind) /usr/local/bin/fd
 echo "Installing additional plugin dependencies..."
 sudo apt-get install -y gcc make
 
-echo "Installation complete! You can now start Neovim."
+luarocks install luasocket
 
-# Set ownership of all created files to vscode user
-sudo chown -R vscode:vscode /home/vscode/.config
+echo "Installation complete! You can now start Neovim."
